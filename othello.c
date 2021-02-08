490d6819541ac7a3b6b343cce2ad20f3fa308d4b
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-02-08 16:32:25 -0500

    init

---
+++ othello.c
@@ -0,0 +1,84 @@
+//***************************************************
+// Filename: othello.c
+//
+// Author(s): 
+//***************************************************
+
+#include "othello.h"
+
+// Constructs and returns a string representation of the board
+char *toString(int size, char board[][size])
+{
+	char *str = (char *) calloc(3 * (size+1) * (size+1), sizeof(char));
+	char *ptr = str;
+
+	for (int i = 0; i < size; i++) {
+		ptr += sprintf(ptr,(i == 0 ? "%3d" : "%2d"),i+1);
+	}
+	sprintf(ptr,"\n");
+	ptr++;
+
+	for (int i = 0; i < size; i++) {
+		sprintf(ptr,"%1d",i+1);
+		ptr++;
+		for (int j = 0; j < size; j++) {
+			sprintf(ptr,"%2c",board[i][j]);
+			ptr += 2;
+		}
+		sprintf(ptr,"\n");
+		ptr++;
+	}
+
+	return str;
+}
+
+// Initializes the board with start configuration of discs (see project specs)
+void initializeBoard(int size, char board[][size])
+{
+	// COMPLETE THIS FUNCTION
+}
+
+// Returns true if moving the disc to location row,col is valid; false otherwise
+bool isValidMove(int size, char board[][size], int row, int col, char disc)
+{
+	return true;	// REPLACE THIS WITH YOUR IMPLEMENTATION
+}
+
+// Places the disc at location row,col and flips the opponent discs as needed
+void placeDiscAt(int size, char board[][size], int row, int col, char disc)
+{
+	if (!isValidMove(size,board,row,col,disc)) {
+		return;
+	}
+
+	// COMPLETE REST OF THIS FUNCTION
+}
+
+// Returns true if a valid move for disc is available; false otherwise
+bool isValidMoveAvailable(int size, char board[][size], char disc)
+{
+	return true;	// REPLACE THIS WITH YOUR IMPLEMENTATION
+}
+
+// Returns true if the board is fully occupied with discs; false otherwise
+bool isBoardFull(int size, char board[][size])
+{
+	return false;	// REPLACE THIS WITH YOUR IMPLEMENTATION	
+}
+
+// Returns true if either the board is full or a valid move is not available for either disc; false otherwise
+bool isGameOver(int size, char board[][size])
+{
+	return false;	// REPLACE THIS WITH YOUR IMPLEMENTATION
+}
+
+// If there is a winner, it returns the disc (BLACK or WHITE) associated with the winner.
+// In case of a tie, it returns TIE. When called before the game is over, it returns 0.
+char checkWinner(int size, char board[][size])
+{
+	if (!isGameOver(size,board)) {
+		return 0;
+	}
+
+	return TIE;	// REPLACE THIS WITH YOUR IMPLEMENTATION
+}
